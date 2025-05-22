c77f2099221def38f361b6a6b8675dd485052b3b
Yuşa Bozkuş <yusa4456bozkus@gmail.com>
2025-05-22 05:09:47 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
